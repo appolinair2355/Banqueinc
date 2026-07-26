c1df204bb29e680ec956a938457ccec9a9f11413
appolinair2355 <felicitationsbien990@gmail.com>
2026-07-26 14:56:52 +0100

    Add files via upload

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -9,19 +9,14 @@
 // Additionally, you should also exclude this file from your linter and/or formatter to prevent it from being checked or modified.
 
 import { Route as rootRouteImport } from './routes/__root'
-import { Route as WafacashRouteImport } from './routes/wafacash'
-import { Route as CihRouteImport } from './routes/cih'
-import { Route as AttijariRouteImport } from './routes/attijari'
 import { Route as IndexRouteImport } from './routes/index'
+import { Route as AttijariRouteImport } from './routes/attijari'
+import { Route as CihRouteImport } from './routes/cih'
+import { Route as WafacashRouteImport } from './routes/wafacash'
 
-const WafacashRoute = WafacashRouteImport.update({
-  id: '/wafacash',
-  path: '/wafacash',
-  getParentRoute: () => rootRouteImport,
-} as any)
-const CihRoute = CihRouteImport.update({
-  id: '/cih',
-  path: '/cih',
+const IndexRoute = IndexRouteImport.update({
+  id: '/',
+  path: '/',
   getParentRoute: () => rootRouteImport,
 } as any)
 const AttijariRoute = AttijariRouteImport.update({
@@ -29,9 +24,14 @@ const AttijariRoute = AttijariRouteImport.update({
   path: '/attijari',
   getParentRoute: () => rootRouteImport,
 } as any)
-const IndexRoute = IndexRouteImport.update({
-  id: '/',
-  path: '/',
+const CihRoute = CihRouteImport.update({
+  id: '/cih',
+  path: '/cih',
+  getParentRoute: () => rootRouteImport,
+} as any)
+const WafacashRoute = WafacashRouteImport.update({
+  id: '/wafacash',
+  path: '/wafacash',
   getParentRoute: () => rootRouteImport,
 } as any)
 
@@ -71,18 +71,11 @@ export interface RootRouteChildren {
 
 declare module '@tanstack/react-router' {
   interface FileRoutesByPath {
-    '/wafacash': {
-      id: '/wafacash'
-      path: '/wafacash'
-      fullPath: '/wafacash'
-      preLoaderRoute: typeof WafacashRouteImport
-      parentRoute: typeof rootRouteImport
-    }
-    '/cih': {
-      id: '/cih'
-      path: '/cih'
-      fullPath: '/cih'
-      preLoaderRoute: typeof CihRouteImport
+    '/': {
+      id: '/'
+      path: '/'
+      fullPath: '/'
+      preLoaderRoute: typeof IndexRouteImport
       parentRoute: typeof rootRouteImport
     }
     '/attijari': {
@@ -92,11 +85,18 @@ declare module '@tanstack/react-router' {
       preLoaderRoute: typeof AttijariRouteImport
       parentRoute: typeof rootRouteImport
     }
-    '/': {
-      id: '/'
-      path: '/'
-      fullPath: '/'
-      preLoaderRoute: typeof IndexRouteImport
+    '/cih': {
+      id: '/cih'
+      path: '/cih'
+      fullPath: '/cih'
+      preLoaderRoute: typeof CihRouteImport
+      parentRoute: typeof rootRouteImport
+    }
+    '/wafacash': {
+      id: '/wafacash'
+      path: '/wafacash'
+      fullPath: '/wafacash'
+      preLoaderRoute: typeof WafacashRouteImport
       parentRoute: typeof rootRouteImport
     }
   }
@@ -111,3 +111,13 @@ const rootRouteChildren: RootRouteChildren = {
 export const routeTree = rootRouteImport
   ._addFileChildren(rootRouteChildren)
   ._addFileTypes<FileRouteTypes>()
+
+import type { getRouter } from './router.tsx'
+import type { startInstance } from './start.ts'
+declare module '@tanstack/react-start' {
+  interface Register {
+    ssr: true
+    router: Awaited<ReturnType<typeof getRouter>>
+    config: Awaited<ReturnType<typeof startInstance.getOptions>>
+  }
+}
